ad5a25f2721ae5942a560cc124bd5073ceba4ac2
risou <zarisoume@gmail.com>
2024-01-03 13:43:06 +0700

    functions for table entries in LaTeX

--- smalllab/table.py
+++ smalllab/table.py
@@ -21,6 +21,78 @@ def table_h(H: list, *L: list) -> str:
     return s
 
 
+def table_h2(H: list, *L: list) -> str:
+    """Return html table with heading H and rows *L = L1, l2, ..., Ln.
+    If there is no heading, enter empty list as the first argument.
+    The heading is displayed as row.
+    """
+    list_count = len(L)
+    elem_count = len(L[0])
+    
+    s = "<table>"
+    if H:
+        s += "<tr>"
+        for h in H:
+            s += "<th>" + str( h ) + "</th>"
+        s += "</tr>"
+    
+    for j in range(list_count):
+        s += "<tr>"
+        for k in range(elem_count):
+            s += "<td>" + str( L[j][k] ) + "</td>"
+        s += "</tr>"
+        
+    s += "</table>"
+    return s
+
+
+def table_h_tex(H: list, *L: list) -> str:
+    """Return html table with heading H and rows *L = L1, l2, ..., Ln.
+    If there is no heading, enter empty list as the first argument.
+    All entries are displayed in LaTeX.
+    """
+    list_count = len(L)
+    elem_count = len(L[0])
+    
+    s = "<table>"
+    for j in range(list_count):
+        s += "<tr>"
+        if H:
+            s += "<th>" + str( H[j] ) + "</th>"
+        for k in range(elem_count):
+            s += "<td>$" + str( L[j][k] ) + "$</td>"
+        s += "</tr>"
+        
+    s += "</table>"
+    return s
+
+
+def table_h2_tex(H: list, *L: list) -> str:
+    """Return html table with heading H and rows *L = L1, l2, ..., Ln.
+    If there is no heading, enter empty list as the first argument.
+    The heading is displayed as row.
+    All entries are displayed in LaTeX.
+    """
+    list_count = len(L)
+    elem_count = len(L[0])
+    
+    s = "<table>"
+    if H:
+        s += "<tr>"
+        for h in H:
+            s += "<th>" + str( h ) + "</th>"
+        s += "</tr>"
+    
+    for j in range(list_count):
+        s += "<tr>"
+        for k in range(elem_count):
+            s += "<td>$" + str( L[j][k] ) + "$</td>"
+        s += "</tr>"
+        
+    s += "</table>"
+    return s
+
+
 def table_v(H: list, *L: list) -> str:
     """Return html table with heading H and columns *L = L1, l2, ..., Ln.
     If there is no heading, enter empty list as the first argument.
@@ -39,6 +111,76 @@ def table_v(H: list, *L: list) -> str:
         s += "<tr>"
         for k in range(list_count):
             s += "<td>" + str( L[k][j] ) + "</td>"
+        s += "</tr>"  
+        
+    s += "</table>"
+    return s
+
+
+def table_v2(H: list, *L: list) -> str:
+    """Return html table with heading H and columns *L = L1, l2, ..., Ln.
+    If there is no heading, enter empty list as the first argument.
+    The heading is displayed as column.
+    """
+    list_count = len(L)
+    elem_count = len(L[0])
+    
+    s = "<table>"
+    
+    for j in range(elem_count):
+        s += "<tr>"
+        if H:
+            s += "<th>" + str( H[j] ) + "</th>"
+        for k in range(list_count):
+            s += "<td>" + str( L[k][j] ) + "</td>"
+        s += "</tr>"  
+        
+    s += "</table>"
+    return s
+
+
+def table_v_tex(H: list, *L: list) -> str:
+    """Return html table with heading H and columns *L = L1, l2, ..., Ln.
+    If there is no heading, enter empty list as the first argument.
+    All entries are displayed in LaTeX.
+    """
+    list_count = len(L)
+    elem_count = len(L[0])
+    
+    s = "<table>"
+    if H:
+        s += "<tr>"
+        for h in H:
+            s += "<th>" + str( h ) + "</th>"
+        s += "</tr>"
+    
+    for j in range(elem_count):
+        s += "<tr>"
+        for k in range(list_count):
+            s += "<td>$" + str( L[k][j] ) + "$</td>"
+        s += "</tr>"  
+        
+    s += "</table>"
+    return s
+
+
+def table_v2_tex(H: list, *L: list) -> str:
+    """Return html table with heading H and columns *L = L1, l2, ..., Ln.
+    If there is no heading, enter empty list as the first argument.
+    The heading is displayed as column.
+    All entries are displayed in LaTeX.
+    """
+    list_count = len(L)
+    elem_count = len(L[0])
+    
+    s = "<table>"
+    
+    for j in range(elem_count):
+        s += "<tr>"
+        if H:
+            s += "<th>" + str( H[j] ) + "</th>"
+        for k in range(list_count):
+            s += "<td>$" + str( L[k][j] ) + "$</td>"
         s += "</tr>"  
         
     s += "</table>"
