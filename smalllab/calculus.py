2f73a6b237a6389fb1663a3615a15a691e6429b0
risou <zarisoume@gmail.com>
2024-01-07 22:56:22 +0700

    convert list into polynomial or string

--- smalllab/calculus.py
+++ smalllab/calculus.py
@@ -1,8 +1,96 @@
 """This module contains functions related to calculus.
 """
 
+from collections.abc import Callable
+
 number = int | float
 
 def sign(x: number) -> int:
     """Return -1 if x < 0, 0 if x = 0, 1 if x > 0."""
     return (x > 0) - (x < 0)
+
+
+def list2poly(L: list[number]) -> Callable[[number], float]:
+    """Return a polynomial function with coefficient in L.
+    The coefficient of highest-degree term is the first item in list.
+
+    Parameters
+    ----------
+    L : list of integer or float
+    """
+    if len(L) == 0:
+        raise ValueError("the list cannot be empty")
+        
+    def p(x: number) -> float:
+        deg = len(L) - 1 # the degree of polynomial
+        y = 0.0
+        for j, coef in enumerate(L):
+            y += x**(deg-j) * coef
+        return y
+    return p
+
+
+def format_poly(deg: int, coef: number, leading: bool = False) -> str:
+    """Return formatted string for a polynomial term.
+    
+    Parameters
+    ----------
+    deg : int
+        The power of coefficient
+    coef : int or float
+    leading : bool, default=False
+        Whether or not this coefficient is a leading coefficient
+    """
+    if deg < 0:
+        raise ValueError("degree must be nonnegative")
+
+    # If not a zero polynomial, don't display 0
+    if coef == 0:
+        if leading and deg == 0:
+            return '0'
+        return ''
+
+    if deg == 0:
+        if leading:
+            return f"{coef}"
+        if coef < 0:
+            return f"{coef}"
+        return f"+{coef}"
+
+    # Do not show the power if deg = 1
+    if deg == 1:
+        if leading:
+            return f"{coef}x"
+        if coef < 0:
+            return f"{coef}x"
+        return f"+{coef}x"
+
+    # For degree > 1
+    if leading:
+        return f"{coef}x^{deg}"
+    if coef < 0:
+        return f"{coef}x^{deg}"
+    return f"+{coef}x^{deg}"
+
+
+def list2polystr(L_: list) -> str:
+    """Return a string representing polynomial function with coefficient in L_.
+    The coefficient of highest-degree term is the first item in list.
+
+    Parameters
+    ----------
+    L_ : list of integer or float
+    """
+    L = L_.copy()
+    
+    if len(L) == 0:
+        return ''
+    if len(L) == 1:
+        return str(L[0])
+    
+    deg = len(L) - 1
+    L[0] = format_poly(deg, L[0], True)
+    for j in range(1, deg+1):
+        L[j] = format_poly(deg-j, L[j])
+
+    return ''.join(L)
--- tests/test_calculus.py
+++ tests/test_calculus.py
@@ -11,5 +11,72 @@ class TestSign(unittest.TestCase):
         self.assertEqual(-1, sign(-1.3))
 
 
+class TestList2Poly(unittest.TestCase):
+    # smalllab.calculus.list2poly
+    
+    def test_some_values(self):
+        f1 = lambda x: -3*x**3 - 2*x + 17
+        L = [-3, 0, -2, 17]
+        f2 = list2poly(L)
+        for x in range(5):
+            self.assertEqual(f1(x), f2(x))
+
+    def test_cannot_be_empty(self):
+        with self.assertRaises(ValueError):
+            list2poly([])
+
+
+class TestFormatPoly(unittest.TestCase):
+    # smalllab.calculus.format_poly
+    
+    def test_dont_display_0(self):
+        self.assertEqual('', format_poly(3, 0))
+        self.assertEqual('', format_poly(3, 0, True))
+    
+    def test_display_0_for_zero_polynomial(self):
+        self.assertEqual('0', format_poly(0, 0, True))
+
+    def test_degree_zero(self):
+        self.assertEqual('', format_poly(0, 0))
+        self.assertEqual('0', format_poly(0, 0, True))
+        self.assertEqual('+2', format_poly(0, 2))
+        self.assertEqual('2', format_poly(0, 2, True))
+        self.assertEqual('-2', format_poly(0, -2))
+    
+    def test_degree_1(self):
+        self.assertEqual('', format_poly(1, 0))
+        self.assertEqual('+2x', format_poly(1, 2))
+        self.assertEqual('2x', format_poly(1, 2, True))
+        self.assertEqual('-2x', format_poly(1, -2))
+    
+    def test_higher_degree(self):
+        self.assertEqual('', format_poly(2, 0))
+        self.assertEqual('+2x^2', format_poly(2, 2))
+        self.assertEqual('2x^2', format_poly(2, 2, True))
+        self.assertEqual('-2x^2', format_poly(2, -2))
+    
+    def test_degree_must_be_nonnegative(self):
+        with self.assertRaises(ValueError):
+            format_poly(-1, 3)
+
+
+class TestList2PolyStr(unittest.TestCase):
+    # smalllab.calculus.list2polystr
+
+    def test_degree_less_than_1(self):
+        self.assertEqual('', list2polystr([]))
+        self.assertEqual('0', list2polystr([0]))
+        self.assertEqual('-1.3', list2polystr([-1.3]))
+    
+    def test_degree_1(self):
+        self.assertEqual('1x+1', list2polystr([1, 1]))
+        self.assertEqual('-2x-2', list2polystr([-2, -2]))
+        self.assertEqual('2x', list2polystr([2, 0]))
+    
+    def test_higher_degree(self):
+        self.assertEqual('3x^2-2x+1', list2polystr([3, -2, 1]))
+        self.assertEqual('-3x^6+2x^3-1x^2', list2polystr([-3, 0, 0, 2, -1, 0, 0]))
+
+
 if __name__ == "__main__":
     unittest.main()
